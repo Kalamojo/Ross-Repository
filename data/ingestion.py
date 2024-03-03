0cf6a77fae6fd9bed6636826b5b071280cd3438e
Kalamojo <kojo22444488888888@gmail.com>
2024-03-02 23:39:48 -0600

    Exploring data ingestion

---
+++ data/ingestion.py
@@ -0,0 +1,58 @@
+import os
+import sqlite3
+import pandas as pd
+from scraper import LawScraper
+from datetime import datetime
+import json
+
+def print_table(db, name):
+    print(name + ':')
+    res_table = db.execute(f'SELECT * FROM {name}')
+    rows = [[item[0] for item in res_table.description]] + [row for row in res_table]
+    lens = []
+    for col in zip(*rows):
+        lens.append(min(max([len(str(v)) for v in col]), 30))
+    format = "  ".join(["{:." + str(l) + "}" for l in lens])
+    for row in rows:
+        row = [str(x) for x in row]
+        print(format.format(*row))
+    print('')
+
+#"""
+scraper = LawScraper(headless=True)
+start_date = datetime(1760, 1, 1)
+end_date = datetime(2024, 12, 31)
+caseDict = scraper.get_cases(start_date, end_date, 10)
+#"""
+
+"""
+with open("tempCases.json", 'r') as f:
+    caseDict = json.load(f)
+#"""
+
+#os.remove("./courtCases.db")
+
+con = sqlite3.connect('courtCases.db')
+cur = con.cursor()
+
+### Musicians table
+
+command = """
+CREATE TABLE Cases (
+    name text,
+    citation text,
+    docket_no text,
+    decision_date date,
+    court text,
+    content text,
+    PRIMARY KEY (citation)
+);
+"""
+cur.execute(command)
+#"""
+for case in caseDict:
+    cur.execute("INSERT INTO Cases VALUES (?, ?, ?, ?, ?, ?)", 
+                (case['name'], case.get("Citation", None), case.get("Docket No", None), case['Decided'], case['Court'], case['content']))
+#"""
+con.commit()
+print_table(cur, "Cases")
---
+++ data/scraper.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+
+class LawScraper:
+    def __init__(self, headless: bool = True, driverPath : str = '/snap/bin/geckodriver') -> None:
+        self.base_url = "https://caselaw.findlaw.com/search.html?search_type=party&court=us-supreme-court&text="
+        self.options = webdriver.FirefoxOptions()
+        self.options.add_argument('--no-sandbox')
+        self.options.add_argument('--disable-dev-shm-usage')
+        if headless: self.options.add_argument("--headless")
+        self.driverService = Service(driverPath)
+        #self.driver = webdriver.Firefox(service=self.driverService, options=self.options)
+    
+    def get_cases(self, start: datetime, end: datetime, pageLimit: int = 20000) -> list[dict]:
+        caseList = []
+        self.driver = webdriver.Firefox(service=self.driverService, options=self.options)
+        self.driver.get(self.base_url + f"&date_start={start.strftime("%Y%m%d")}&date_end={end.strftime("%Y%m%d")}")
+        span = self.driver.find_element(By.CLASS_NAME, 'pagecount')
+        count = span.find_elements(By.TAG_NAME, 'strong')[1]
+        pageCount = min(int(count.get_attribute("innerText")), pageLimit)
+        self.driver.quit()
+        print(pageCount)
+        for p in range(1, pageCount+1):
+            if p % 1000 == 1:
+                print('On Page:', p)
+            urls = self.get_page_urls(start, end, p)
+            print(urls)
+            for url in urls:
+                caseList.append(self.get_case_content(url))
+        return caseList
+
+    def get_page_urls(self, start: datetime, end: datetime, pageNum : int) -> list[str]:
+        self.driver = webdriver.Firefox(service=self.driverService, options=self.options)
+        self.driver.get(self.base_url + f"&date_start={start.strftime("%Y%m%d")}&date_end={end.strftime("%Y%m%d")}&page={pageNum}")
+        table = self.driver.find_element(By.CLASS_NAME, 'responsive-card-table')
+        linkTagList = table.find_elements(By.TAG_NAME, 'a')
+        urlList = [a.get_attribute("href") for a in linkTagList]
+        self.driver.quit()
+        return urlList
+    
+    def get_case_content(self, url: str) -> dict:
+        caseContent = {}
+        self.driver = webdriver.Firefox(service=self.driverService, options=self.options)
+        self.driver.get(url)
+        info = self.driver.find_element(By.CLASS_NAME, 'case-information')
+        title = info.find_element(By.TAG_NAME, 'h2').get_attribute("innerText")
+        caseContent['name'] = ', '.join(title.split(', ')[:-1]).split('(')[0].rstrip()
+
+        paragraphs = info.find_elements(By.TAG_NAME, 'p')
+        for paragraph in paragraphs:
+            paragraphText = paragraph.get_attribute("innerText").split(':')
+            caseContent[paragraphText[0]] = paragraphText[1]
+            
+        content = self.driver.find_element(By.ID, 'caselaw-content')
+        caseContent['content'] = content.get_attribute("innerText")
+        self.driver.quit()
+        return caseContent
